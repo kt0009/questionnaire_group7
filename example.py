00e11200d78b6f8db3be3e209d44a99c3ff5d179
Carl Shan <shan.carl@gmail.com>
2013-10-04 23:49:11 -0700

    Carls changes and curation

--- example.py
+++ example.py
@@ -63,7 +63,7 @@ print docid
 
 # <codecell>
 
-fieldnames = ['Timestamp','What is your learning style?']
+fieldnames = ['Timestamp','What is your learning style?', 'Which department and course numbers (e.g. Stat 157)?']
 print fieldnames
 
 # <markdowncell>
@@ -83,6 +83,37 @@ print "Number of rows: {}".format(len(filtered_data))
 
 # <codecell>
 
-for row in take(3, filtered_data):
-    print row
+### Cleaning up filtered_data
+import re
+import csv
+
+## Removing unicode
+filtered_data[17][fieldnames[2]] = re.sub(u'\u2022', '', filtered_data[17][fieldnames[1]])
+filtered_data[24][fieldnames[2]] = re.sub(u'\u2013', '', filtered_data[24][fieldnames[1]])
+
+def clean_classes(string):
+	s = []
+	for elem in ('133', '134', '135'):
+		if elem in string:
+			s.append('Stat ' + elem)
+	return(s)
+
+## Creates Cleaned Dictionary to be parsed into .csv
+cleaned_data = []
+for elem in filtered_data:
+	if 'Kinesthetic' in elem[fieldnames[1]]: # checks to see if the cell is non-empty and machine readable
+		scores = re.findall('\d+', elem[fieldnames[1]])
+		row_dict = {'Timestamp' : elem['Timestamp'], 'Visual' : scores[0], 'Aural' : scores[1], 'Read/Write' : scores[2], 'Kinesthetic': scores[3], 'Stat 133' : '0', 'Stat 134' : '0', 'Stat 135' : '0'}
+		classes = clean_classes(elem[fieldnames[2]]) # only keeps Stat 133, Stat 134 and Stat 135
+		for elem in ('Stat 133', 'Stat 134', 'Stat 135'):
+			if elem in classes:
+				row_dict[elem] = 1
+		cleaned_data.append(row_dict)
+
+# Creates .csv file with cleaned data
+names = ['Timestamp', 'Aural', 'Kinesthetic', 'Read/Write', 'Visual', 'Stat 133', 'Stat 134', 'Stat 135']
+f = open('data.csv', 'wb')
+dict_writer = csv.DictWriter(f, names, restval='NA')
+dict_writer.writeheader()
+dict_writer.writerows(cleaned_data)
 
