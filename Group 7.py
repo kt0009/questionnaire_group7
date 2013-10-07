1facf5be581f58e9589f641568229100535e2dc2
sunnymh <sunnymh2009@gmail.com>
2013-10-06 17:08:49 -0700

    Visulization done

---
+++ Group 7.py
@@ -0,0 +1,231 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <headingcell level=1>
+
+# Data Science: Group 7 
+
+# <rawcell>
+
+# Carl Shan, He Ma, Alyssa Parker, Vincent Canlas
+
+# <headingcell level=2>
+
+# Step 0: Setup
+
+# <markdowncell>
+
+# Import basic Python libraries for use in your program: [os.path](http://docs.python.org/2/library/os.path.html) and [ConfigParser](http://docs.python.org/2/library/configparser.html). We also imported re and csv for regular expression parsing and creating csv files respectively.
+
+# <codecell>
+
+import os.path
+import ConfigParser
+import re
+import csv
+
+# <markdowncell>
+
+# An example of reading data from a Google Spreadsheet using the gspread library: http://stackoverflow.com/a/18296318/462302
+# 
+# First you'll need to install the gspread library on your virtual machine using: `sudo pip install gspread`
+
+# <codecell>
+
+import gspread
+
+# <markdowncell>
+
+# Define `take(n, iterable)` which is a convenience function to limit the amount of output that you print. Useful when you have lots of data that will clutter up your screen!
+
+# <codecell>
+
+from itertools import islice
+def take(n, iterable):
+    "Return first n items of the iterable as a list"
+    return list(islice(iterable, n))
+
+# <markdowncell>
+
+# Read the username and password from the `[google]` section of the `stat157.cfg` config file from your virtual machine home directory.
+
+# <codecell>
+
+home = os.path.expanduser("~")
+configfile = os.path.join(home, 'stat157.cfg')
+config = ConfigParser.SafeConfigParser()
+config.read(configfile)
+username = config.get('google', 'username')
+password = config.get('google', 'password')
+print username
+
+# <markdowncell>
+
+# Read the docid of the Google Spreadsheet from the config file.
+
+# <codecell>
+
+docid = config.get('questionnaire', 'docid')
+client = gspread.login(username, password)
+spreadsheet = client.open_by_key(docid)
+worksheet = spreadsheet.get_worksheet(0)
+print docid
+
+# <markdowncell>
+
+# Add field names to this list to include the column from the Google Spreadsheet in the filtered data output. You should choose one other column in addition to the learning style column. Refer to README.md from the homework assignment.
+
+# <codecell>
+
+fieldnames = ['Timestamp','What is your learning style?', 'Which department and course numbers (e.g. Stat 157)?']
+print fieldnames
+
+# <markdowncell>
+
+# Read in ALL rows of data from the Google Spreadsheet, but filter out columns that are not listed in `fieldnames`.
+
+# <codecell>
+
+filtered_data = []
+for row in worksheet.get_all_records():
+    filtered_data.append({k:v for k,v in row.iteritems() if k in fieldnames})
+print "Number of rows: {}".format(len(filtered_data))
+
+# <headingcell level=2>
+
+# Step 1 - Curation
+
+# <headingcell level=3>
+
+# Part A - Cleaning up Data
+
+# <codecell>
+
+def clean_classes(string):
+    'This function cleans the list of classes, pruning it to just include the Stat 130s'
+    s = []
+    for elem in ('133', '134', '135'):
+        if elem in string:
+            s.append('STAT' + elem)
+    return(s)
+
+## Removing unicode from corpus
+filtered_data[17][fieldnames[2]] = re.sub(u'\u2022', '', filtered_data[17][fieldnames[1]])
+filtered_data[24][fieldnames[2]] = re.sub(u'\u2013', '', filtered_data[24][fieldnames[1]])
+
+## Creates Cleaned Dictionary to be parsed into .csv
+cleaned_data = []
+for elem in filtered_data:
+    if 'Kinesthetic' in elem[fieldnames[1]]: # checks to see if the cell is non-empty and machine readable
+        scores = re.findall('\d+', elem[fieldnames[1]])
+        row_dict = {'Timestamp' : elem['Timestamp'], 'Visual' : scores[0], 'Aural' : scores[1], 'Read_Write' : scores[2], 'Kinesthetic': scores[3], 'STAT133' : '0', 'STAT134' : '0', 'STAT135' : '0', 'CS' : '0'}
+        classes = clean_classes(elem[fieldnames[2]]) # only keeps Stat 133, Stat 134 and Stat 135
+        for course in ('STAT133', 'STAT134', 'STAT135'):
+			if course in classes:
+				row_dict[course] = 1
+        if re.match(r'[Cc][Ss] ?[0-9]+',elem[fieldnames[2]]): # if the person has taken as CS classes
+            row_dict['CS'] = 1
+        cleaned_data.append(row_dict)
+
+# <headingcell level=3>
+
+# Part B - Creating .csv File
+
+# <codecell>
+
+## Creates a .csv file called data.csv from cleaned data
+names = ['Timestamp', 'Aural', 'Kinesthetic', 'Read_Write', 'Visual', 'STAT133', 'STAT134', 'STAT135', 'CS']
+f = open('data.csv', 'wb')
+dict_writer = csv.DictWriter(f, names, restval='NA')
+dict_writer.writeheader()
+dict_writer.writerows(cleaned_data)
+
+# <headingcell level=2>
+
+# Step 2 - Analysis
+
+# <codecell>
+
+# code start here
+
+# <headingcell level=2>
+
+# Step3 - Visualization
+
+# <rawcell>
+
+# Everything is done with R, please refer to visulization.R for implementation.
+
+# <headingcell level=3>
+
+# Part A: Learning Style
+
+# <headingcell level=4>
+
+# Distribution of the scores for Aural, Kinesthetic, Read_Write, Visual
+
+# <markdowncell>
+
+# <img src="files/hist_style.png">
+# <img src="files/violin_style.png">
+
+# <rawcell>
+
+# Due to the limitation of the size of the data, we can't really say much about the data. 
+# Aural score spread out between 1 and 11, while the other three scores have a larger spread.
+# Seems like the scores could fit with normal distribution.
+
+# <headingcell level=4>
+
+# Normal distribution fitness
+
+# <markdowncell>
+
+# <img src="files/normal_style.png">
+
+# <rawcell>
+
+# Since the data is ordinal, this plot can only be taken as a reference.
+# Aural has big tail for both sides. 
+# The other three are approximately normal, but skew to the side.
+
+# <headingcell level=4>
+
+# Pairsie correlation for learning style
+
+# <markdowncell>
+
+# <img src="files/scatterplot_style.png">
+
+# <rawcell>
+
+# All seems to be evenly spread out.
+
+# <markdowncell>
+
+# <img src="files/heatmap_style.png">
+
+# <rawcell>
+
+# Again, since the data is ordinal, Pearson's correlation might not be a accurate prediction.
+# Still, we can see that Read_Write is not correlatied with the other three categories.
+# For Aural, Kinesthetic, and Visual, if the student score higher in one of them, he is likely to score higher in the other two.
+
+# <headingcell level=3>
+
+# Part B: Classes taken
+
+# <headingcell level=4>
+
+# Distribution of people taking STAT and CS class
+
+# <markdowncell>
+
+# <img src="files/barplot_class.png">
+# <img src="files/stat_cs_class.png">
+
+# <rawcell>
+
+# There might be some correlation between learning style and the different classes people take.
+# But due to the limitation of the sample size, it is not likely we can get to a conclusion
+
--- visualization.R
+++ visualization.R
@@ -1,30 +1,69 @@
 dir = '/home/sunnymh/stat157/questionnaire_group7/'
 
+library(vioplot)
 library(pheatmap)
+library(plotrix)
 
 mydata = read.csv(paste(dir, 'data.csv', sep = ''), colClasses=c("character",rep("numeric",8)))
-
 learing_cats = c('Aural', 'Kinesthetic', 'Read_Write', 'Visual')
+class_cats = c('STAT133', 'STAT134', 'STAT135', 'CS')
 
-# Distribution of learning style
-png(paste(dir, 'violin.png', sep = ''), 1200, 900)
-library(vioplot)
+##################
+# Learning Style #
+##################
+
+# 1. Distribution of learning style
+png(paste(dir, 'hist_style.png', sep = ''), 500, 400)
+with(mydata, 
+     multhist(list(Aural,Kinesthetic,Read_Write,Visual), 
+              main = "Learning style", xlab = "score", ylab = "count", 
+              col = c("yellow", "red", "blue", "green")))
+legend("topright", learing_cats, fill = c("yellow", "red", "blue", "green"), cex = 0.8)
+dev.off()
+
+png(paste(dir, 'violin_style.png', sep = ''), 500, 400)
 with(mydata, 
      vioplot(Aural,Kinesthetic,Read_Write,Visual, names = learing_cats, col = "lightblue"))
 title("Violin Plots for learning style")
 dev.off()
 
-# Pairsie correlation for learning style
-png(paste(dir, 'scatterplot.png', sep = ''), 1200, 900)
+# Conclusions:
+# Due to the limit of the size of the data, we can't really say much about the data. 
+# Still Aural score seems to be evenly spread out from 0 to 10.
+# The other three scores has a larger spread.
+# Seems like the scores could fit with a normal distribution.
+
+# 2. Check how good each parameter fits with normal distribution.
+check_normal = function(data, name){
+  data.norm = (data - mean(data))/sd(data)
+  qqnorm(data.norm, xlim = c(-1,1), ylim = c(-1,1),  main = paste("Normal Q-Q Plot for ", name), cex = 0.5, pch = 19)
+  abline(0, 1)
+}
+png(paste(dir, 'normal_style.png', sep = ''), 900, 800)
+par(mfrow=c(2,2))
+check_normal(mydata$Aural, 'Aural')
+check_normal(mydata$Kinesthetic, 'Kinesthetic')
+check_normal(mydata$Read_Write, 'Read_Write')
+check_normal(mydata$Visual, 'Visual')
+dev.off()
+# Conclusions:
+# Since the data is ordinal, this plot can only be taken as a reference.
+# As we can see from violin plot, Aural has big tail for both sides. 
+# The other three are approximately normal, but skew to the side.
+
+# 3. Pairsie correlation for learning style
+png(paste(dir, 'scatterplot_style.png', sep = ''), 900, 800)
 pairs(~Aural+Kinesthetic+Read_Write+Visual, data = mydata, main = 'Scatterplot Matrix for learning style', cex =0.5, pch = 19)
 dev.off()
-png(paste(dir, 'heatmap.png', sep = ''), 1200, 900)
+
 person_cor = matrix(nrow = length(learing_cats), ncol = length(learing_cats), byrow = TRUE, dimnames = list(learing_cats, learing_cats) )
 for (var1 in learing_cats){
   for (var2 in learing_cats){
     person_cor[var1, var2] = cor(mydata[[var1]], mydata[[var2]], use = "complete.obs", method = "pearson")
   }
 }
+
+png(paste(dir, 'heatmap_style.png', sep = ''), 500, 400)
 pheatmap(person_cor, clustering_distance_rows=as.dist((1-person_cor^2)), clustering_distance_cols=as.dist(1-person_cor^2), 
          cluster_rows=T, cluster_cols = T, 
          breaks=seq(-1, 1, 0.1),
@@ -33,9 +72,55 @@ pheatmap(person_cor, clustering_distance_rows=as.dist((1-person_cor^2)), cluster
          main = paste("Person's correlation for learning style"), display_numbers = T, fontsize=14)
 dev.off()
 
-a = sapply(mydata[, c("STAT133","STAT134", "STAT135", "CS")], sum)
-barplot(a)
+# Again, since the data is ordinal, Pearson's correlation might not be a accurate prediction.
+# Still, we can see that Read_Write is not correlatied with the other three categories.
+# For Aural, Kinesthetic, and Visual, if the student score higher in one of them, he is likely to score higher in the other two.
 
+###########
+# Classes #
+###########
 
-hist(factor(mydata$STAT133)
-     
+# barplot for distribution of class
+png(paste(dir, 'barplot_class.png', sep = ''), 500, 400)
+barplot(sapply(mydata[, class_cats], sum), main = "Barplot for people taking each class")
+dev.off()
+
+############################
+# Learning style & Classes #
+############################
+
+# Distribution of students taking STAT and CS classes 
+png(paste(dir, 'table_class.png', sep = ''), 500, 400)
+stat_group = (mydata$STAT133==1 | mydata$STAT134==1 | mydata$STAT135==1)
+cs_group = mydata$CS==1
+plot(table(stat_group, cs_group), xlab = "People taking STAT class", ylab = "People taking CS class", main = "Distribution of classes taken")
+text(x = c(0.1, 0.6, 0.1, 0.6),y = c(0.05, 0.03, 0.5, 0.5), labels= c(1, 3, 5, 22))
+dev.off()
+
+# Hypothesis:
+# STAT students with a particualr learning style are more likely to take a CS class.
+
+# Assumptions:
+#  Students who have taken at least one of STAT 133, 134, 135 are considered as students with STAT background.
+
+# Method:
+# Compare the learning style of students of group STAT & CS and group STAT & NOT CS.
+
+mydata2 = mydata[stat_group,]
+cat_CS = mydata2$CS == 1
+
+chisq_test = matrix(nrow = length(learing_cats), ncol = length(learing_cats), byrow = TRUE, dimnames = list(learing_cats, learing_cats) )
+for (var1 in learing_cats){
+  for (var2 in learing_cats){
+    test = mydata2[[var1]] > mydata2[[var2]]
+    result = chisq.test(table(cat_CS,test))
+    chisq_test[var1, var2] = result$p.value
+  }
+}
+
+png(paste(dir, 'chisq_class.png', sep = ''), 500, 400)
+pheatmap(chisq_test, color = "white", main = paste("P value for Chi-square test"), display_numbers = T, legend= F, cluster_rows=F, cluster_cols = F, fontsize=14)
+dev.off()
+
+# Conclusion:
+# Due to the size of the sample, chi square test can't tell us anything.
